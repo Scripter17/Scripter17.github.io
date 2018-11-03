b4e3008151e9b640dcacf07896e03538c72323e3
Scripter17 <jameschristopherwise@gmail.com>
2018-11-03 15:53:19 -0400

    Fixed jittering when using tab to navigate the sidebar.

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-/*function age(bday, day){
-	var age;
-	day=day||new Date();
-	age=day.getFullYear()-bday.getFullYear();
-	if (day.getMonth()<bday.getMonth() || (day.getMonth()==bday.getMonth() && day.getDate()<bday.getDate())){age--;}
-	return age;
+// Normally, when you press [tab] when a link in the sidebar is active, the window scrolls to the top. (It's a bug with position:sticky)
+// This script prevents that, albeit with a bit of sphaghetii.
+window.onkeydown=function(e){
+	var ofsYPre, id;
+	if (e.keyCode==9){ // keyCode 9 is [tab]
+		ofsYPre=window.pageYOffset; // The page's Y offset before scrolling
+		window.requestAnimationFrame(function(){ // setTimeout causes 1 millisecond where the body scrolls to the top
+			try{
+				id=document.activeElement.parentElement.parentElement.parentElement.id;
+			} catch (e){
+				// This isn't an isssue, so just fail silently (bad practice, but reasonable here)
+				id=undefined;
+			}
+			// If the current active element is a link in the sidebar, then scroll back to where it was 1 frame ago
+			if (id=="Sidebar"){
+				window.scrollTo(0, ofsYPre)
+			}
+		})
+	}
 }
-window.onload=function(){
-	document.getElementById("age").innerHTML=age(new Date("2002-12-14"))
-}/**/
