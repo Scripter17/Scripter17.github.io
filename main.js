546bafe767100ecb71c5da397138852c268507ad
Scripter17 <jameschristopherwise@gmail.com>
2018-11-11 21:25:40 -0500

    A bunch of stuff
    - Changed color scheme
    - Mobile support is now more subtle
    - Added jokes at the borrom-right of the page
    - Added more stuff to the "About Me" section

--- main.js
+++ main.js
@@ -23,3 +23,31 @@ window.onkeydown=function(e){
 		})
 	}
 }
+
+function randInt(min, max){ // Return values: min to max-1
+	var x=Math.random();
+	return Math.floor(x*(max-min)+min);
+}
+window.onload=function(){
+	var i;
+	terribleJokes=[
+		"eval(Math.random())",
+		"function f(){try{/*stuff*/}catch(e){f();}}",
+		"You were expecting a joke, but it was me, undefined!",
+		"Internet Explorer. That's the joke.",
+		"I'm actually infinitely many monkeys on typewriters."
+	];
+	
+	// Never select the same joke twice in a row. It just seems more random this way, even though it isn't.
+	// Apparently localStorage isn't supported in IE.
+	if (localStorage){
+		do{
+			i=randInt(0,terribleJokes.length);
+		} while (localStorage.getItem("jokeIndex")==i)
+		localStorage.setItem("jokeIndex", i);
+	} else {
+		i=randInt(0,terribleJokes.length);
+	}
+
+	document.getElementById("terribleJoke").innerHTML=terribleJokes[i];
+}
