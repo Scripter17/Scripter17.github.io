7c958bc1773cecf54d3c3d8064f624bcf1bdd07e
Scripter17 <jameschristopherwise@gmail.com>
2019-02-07 20:51:39 -0500

    Time for the annual re-write
    I'm mainly pushing this right now because of the twitter thing in my contacts

--- README.md
+++
@@ -1,4 +0,0 @@
-# Scripter17.github.io
-My GitHub website, I guess.
-
-[scripter17.github.io](https://scripter17.github.io)
--- externalLibraries/update.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set a=Scripter17.github.io\externalLibraries
-cd ..\..
-copy "browser-elements\browserElements.js" "%a%\browserElements.js"
-copy "jwBulkDOM\jwBulkDOM.js" "%a%\jwBulkDOM.js"
-::copy "" "%a%\"
-pause >nul
