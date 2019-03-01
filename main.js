c99ef11288e145dd96efd0d2e0331eaedf62b502
Scripter17 <jameschristopherwise@gmail.com>
2019-03-01 14:52:47 -0500

    Added dark mode

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-// For when I actually need this
+window.onload=function(){
+    if (localStorage.getItem("theme")==undefined){
+        localStorage.setItem("theme", "light");
+    }
+    document.body.setAttribute("theme", localStorage.getItem("theme"));
+    document.getElementById("themeChange").onclick=function(){
+        localStorage.setItem("theme", {"light":"dark", "dark":"light"}[localStorage.getItem("theme")]);
+        document.body.setAttribute("theme", localStorage.getItem("theme"));
+    }
+}
