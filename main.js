544e278f4cfb9c448dc025106fbad0be2d1e46e0
Scripter17 <jameschristopherwise@gmail.com>
2019-04-11 11:33:07 -0400

    Massive clean up effort

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 window.onload=function(){
-    if (localStorage.getItem("theme")==undefined){
-        localStorage.setItem("theme", "light");
-    }
-    document.body.setAttribute("theme", localStorage.getItem("theme"));
-    document.getElementById("themeChange").onclick=function(){
-        localStorage.setItem("theme", {"light":"dark", "dark":"light"}[localStorage.getItem("theme")]);
-        document.body.setAttribute("theme", localStorage.getItem("theme"));
-    }
+	if (localStorage.getItem("theme")==undefined){
+		localStorage.setItem("theme", "light");
+	}
+	document.body.setAttribute("theme", localStorage.getItem("theme"));
+	document.getElementById("themeChange").onclick=function(){
+		localStorage.setItem("theme", {"light":"dark", "dark":"light"}[localStorage.getItem("theme")]);
+		document.body.setAttribute("theme", localStorage.getItem("theme"));
+	}
 }
