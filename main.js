3c2ce498e7e628861e28d28a656ab16e33bdb9dd
Scripter17 <jameschristopherwise@gmail.com>
2019-10-08 18:20:17 -0400

    Apparently I never pushed this
    Whoops

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 window.onload=function(){
+	// Handle themes
 	if (localStorage.getItem("theme")==undefined){
 		localStorage.setItem("theme", "light");
 	}
@@ -7,4 +8,21 @@ window.onload=function(){
 		localStorage.setItem("theme", {"light":"dark", "dark":"light"}[localStorage.getItem("theme")]);
 		document.body.setAttribute("theme", localStorage.getItem("theme"));
 	}
+
+	// Handle the navigation elements
+	var navHttp=new XMLHttpRequest();
+	navHttp.addEventListener("load", function(){
+		var navJSON=JSON.parse(this.responseText), i;
+		for (i in navJSON){
+			var p=window.location.pathname;
+			//console.log(p, p.split("index")[0], i)
+			if (i==p.split("index")[0]){
+				continue
+			}
+			//<a href="/GIMP">GIMP/Photoshop examples</a>
+			document.getElementById("navbar").innerHTML+="<a href='"+i+"'>"+navJSON[i]+"</a>"
+		}
+	})
+	navHttp.open("GET", "/map.json");
+	navHttp.send();
 }
