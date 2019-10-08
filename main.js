0d443b1c7ad373d3534ab400ff5c082adf00a045
Scripter17 <jameschristopherwise@gmail.com>
2019-10-08 19:02:16 -0400

    4 billion IQ move, no jekyll or fancy backend needed
    The URL hash (the bit after the #) now tells the website what stuff to load into to the content div.
    This is an extension on my earlier method of improving the navbar

--- main.js
+++ main.js
@@ -9,20 +9,42 @@ window.onload=function(){
 		document.body.setAttribute("theme", localStorage.getItem("theme"));
 	}
 
+	if (window.location.hash in {"":"", "#":""}){
+		window.location.hash="#main";
+	}
+	handleHash()
+
+	document.getElementById("footer").addEventListener("click",function(){
+		window.scrollTo(0,0);
+	})
+
+
+}
+
+function handleHash(){
+	var request=new XMLHttpRequest();
+	request.addEventListener("load", function(){
+		var html=this.responseText;
+		document.getElementById("main-wrapper").innerHTML=html;
+	})
+	request.open("GET", "pages/"+window.location.hash.substr(1, window.location.hash.length-1)+".html");
+	request.send();
+	handleNavbar();
+}
+function handleNavbar(){
 	// Handle the navigation elements
+	var navbar=document.getElementById("navbar");
 	var navHttp=new XMLHttpRequest();
 	navHttp.addEventListener("load", function(){
+		navbar.innerHTML="";
 		var navJSON=JSON.parse(this.responseText), i;
-		for (i in navJSON){
-			var p=window.location.pathname;
-			//console.log(p, p.split("index")[0], i)
-			if (i==p.split("index")[0]){
-				continue
+		for (var i in navJSON){
+			if (window.location.hash!=i){
+				navbar.innerHTML+="<a href='"+i+"'>"+navJSON[i]+"</a>";
 			}
-			//<a href="/GIMP">GIMP/Photoshop examples</a>
-			document.getElementById("navbar").innerHTML+="<a href='"+i+"'>"+navJSON[i]+"</a>"
 		}
 	})
-	navHttp.open("GET", "/map.json");
+	navHttp.open("GET", "map.json");
 	navHttp.send();
 }
+window.onhashchange=handleHash;
