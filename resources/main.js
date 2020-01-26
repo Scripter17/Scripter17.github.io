d83f4decb48002a43c931c1dbd169b7f52bcae2c
Scripter17 <jameschristopherwise@gmail.com>
2020-01-26 18:16:27 -0500

    Fixed the JavaScript, and also apparently I was editing the output CSS like an idiot

--- resources/main.js
+++ resources/main.js
@@ -1,7 +1,7 @@
 window.onload=function(){
 	var sections=document.querySelectorAll(".content[id] > h2:first-child"),
 		i;
-	for (i in sections){
+	for (i=0; i<sections.length; i++){
 		sections[i].innerHTML+="<a class='anchor' href='#"+sections[i].parentElement.getAttribute("id")+"'>#</a>"
 	}
 }
