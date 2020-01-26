aed0dad0662d2a25ca33616aa8854f4dcae76171
Scripter17 <jameschristopherwise@gmail.com>
2020-01-26 15:11:45 -0500

    Reference commit for before Jekyll

---
+++ main.js
@@ -0,0 +1,16 @@
+/*var sidebarLinks={
+	"Home":"/",
+	"About":"/About",
+	"Contact":"/Contact"
+};
+window.addEventListener("load", function(){
+	var link, shtml="", sidebar;
+	document.body.innerHTML+="<div id='sidebar'><div id='side-nub'></div></div>"
+	sidebar=document.getElementById("sidebar");
+	shtml+="<ul>"
+	for (link in sidebarLinks){
+		shtml+="<li><a href='"+sidebarLinks[link]+"'>"+link+"</a></li>"
+	}
+	shtml+="</ul>"
+	sidebar.innerHTML+=shtml
+});*/
