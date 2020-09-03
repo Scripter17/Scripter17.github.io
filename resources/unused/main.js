fff44aee389374c480d466010e674ea1cbf1ae26
Scripter17 <jameschristopherwise@gmail.com>
2020-09-03 19:48:24 -0400

    Dedicated skills page

--- resources/unused/main.js
+++ resources/unused/main.js
@@ -1,7 +1,7 @@
-window.onload=function(){
+/*window.onload=function(){
 	var sections=document.querySelectorAll(".content[id] > h2:first-child"),
 		i;
 	for (i=0; i<sections.length; i++){
 		sections[i].innerHTML+="<a class='anchor' href='#"+sections[i].parentElement.getAttribute("id")+"'>#</a>"
 	}
-}
+}*/
