ad79c426f2e8f0f0450732047cb6e78bf8850e8c
James C. Wise <jameschristopherwise@gmail.com>
2024-10-09 22:15:31 -0400

    Cleanup in preparation for blogging

--- resources/unused/main.js
+++
@@ -1,7 +0,0 @@
-/*window.onload=function(){
-	var sections=document.querySelectorAll(".content[id] > h2:first-child"),
-		i;
-	for (i=0; i<sections.length; i++){
-		sections[i].innerHTML+="<a class='anchor' href='#"+sections[i].parentElement.getAttribute("id")+"'>#</a>"
-	}
-}*/
